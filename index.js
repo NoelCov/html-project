ede22cec12e0689f43494ff0f6c375a6beebd5bd
Noel Covarrubias <no3lcodes@gmail.com>
2022-11-08 13:45:03 -0800

    Added logo and navbar to project. Also added styles for mobile

---
+++ index.js
@@ -0,0 +1,12 @@
+const linksContainer = document.getElementsByClassName("linksContainer")[0];
+const hamburgerMenu = document.getElementsByClassName("hamburgerMenu")[0];
+
+hamburgerMenu.addEventListener("click", () => {
+    if (linksContainer.classList.contains("inactive")) {
+        linksContainer.classList.remove("inactive");
+        linksContainer.classList.add("active");
+    } else {
+        linksContainer.classList.remove("active");
+        linksContainer.classList.add("inactive");
+    }
+});
