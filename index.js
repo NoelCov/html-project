b2e29bc9ca4804041b2722154ee4c4e155a528ce
Noel Covarrubias <no3lcodes@gmail.com>
2022-11-14 09:20:00 -0800

    Modified index.html to have the script closing tag

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const linksContainer = document.getElementsByClassName("linksContainer")[0];
 const hamburgerMenu = document.getElementsByClassName("hamburgerMenu")[0];
 
+console.log("Hello!");
+
 hamburgerMenu.addEventListener("click", () => {
     if (linksContainer.classList.contains("inactive")) {
         linksContainer.classList.remove("inactive");
