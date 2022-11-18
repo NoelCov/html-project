9faf4d45c7fd0a2d01635bcf3c2c3fe03feeedba
Noel Covarrubias <no3lcodes@gmail.com>
2022-11-18 09:18:03 -0800

    Fixed logo link

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const linksContainer = document.getElementsByClassName("linksContainer")[0];
 const hamburgerMenu = document.getElementsByClassName("hamburgerMenu")[0];
 
-console.log("Hello!");
-
 hamburgerMenu.addEventListener("click", () => {
     if (linksContainer.classList.contains("inactive")) {
         linksContainer.classList.remove("inactive");
